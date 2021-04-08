1c771a2e795765cb65f9ea94d814c6d08187069a
“VarunMakh” <varun@sanraj.net>
2021-04-08 16:48:29 +0530

    Toast message for Recycler view cards

--- app/src/main/java/com/example/fdroid/CustomAdapter.java
+++ app/src/main/java/com/example/fdroid/CustomAdapter.java
@@ -6,7 +6,9 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,6 +18,7 @@ import java.util.ArrayList;
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
     private Context context;
     private ArrayList name,area,category;
+
     CustomAdapter(Context context,ArrayList name,ArrayList area,ArrayList category){
         this.context = context;
         this.name = name;
@@ -31,23 +34,36 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CustomAdapter.MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull CustomAdapter.MyViewHolder holder, final int position) {
+
         holder.restaurantName.setText(String.valueOf(name.get(position)));
         holder.restaurantCategory.setText(String.valueOf(category.get(position)));
         holder.restaurantArea.setText(String.valueOf(area.get(position)));
+        holder.restaurantRowLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context,"Name: " + String.valueOf(name.get(position)), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
+
         return name.size();
+
     }
+
+
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView restaurantName,restaurantCategory,restaurantArea;
+        LinearLayout restaurantRowLayout;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             restaurantName = itemView.findViewById(R.id.restaurantName);
             restaurantCategory = itemView.findViewById(R.id.category);
             restaurantArea = itemView.findViewById(R.id.area);
+            restaurantRowLayout = itemView.findViewById(R.id.restaurantRowLayout);
         }
     }
 }
