3b5458a0ac3b82f7d076279ec543739b4c9fe363
vishv1098 <vishv1098@gmail.com>
2021-04-08 21:57:06 +0530

    Made recycler layout

--- app/src/main/java/com/example/fdroid/SearchFragment.java
+++ app/src/main/java/com/example/fdroid/SearchFragment.java
@@ -1,18 +1,30 @@
 package com.example.fdroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class SearchFragment extends Fragment {
+    Button b1;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search,container,false);
+        View view = inflater.inflate(R.layout.fragment_search,container,false);
+        b1 = view.findViewById(R.id.b1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(),FoodActivity.class);
+                startActivity(i);
+            }
+        });
+        return view;
     }
 }
